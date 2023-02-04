5a33bb405170976e451935215b9884aabaa41775
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2023-02-04 17:44:45 +0530

    cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,7 +12,8 @@ class CartItem extends React.Component {
     // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
   }
-   // testing () {
+
+  // testing () {
   //   const promise = new Promise((resolve, reject) => {
   //     setTimeout(() => {
   //       resolve('done');
@@ -36,34 +37,31 @@ class CartItem extends React.Component {
     // setState form 1
     // this.setState({
     //   qty: this.state.qty + 1
-    // });
+    // }, () => {});
 
     // setState form 2 - if prevState required use this
     this.setState((prevState) => {
       return {
         qty: prevState.qty + 1
       }
-    }, () => {
-      console.log('this.state', this.state);
     });
   }
-  decreaseQuantity = () => {
-    const {qty} = this.state;
 
-    if(qty === 0){
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
       return;
     }
-
     // setState form 2 - if prevState required use this
     this.setState((prevState) => {
       return {
         qty: prevState.qty - 1
       }
-    }, () => {
-      console.log('this.state', this.state);
     });
   }
   render () {
+    console.log('render');
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
