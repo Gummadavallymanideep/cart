ed4eb6c7bf7ad4bc9f614161e7aeef19d2dfe806
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2023-02-06 06:16:39 +0530

    adding firebase

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebaseConfig = {
+    apiKey: "AIzaSyAQL6yFFpLiDcvLovdcAk305iSPWSHQUBU",
+    authDomain: "cart-1ef1a.firebaseapp.com",
+    projectId: "cart-1ef1a",
+    storageBucket: "cart-1ef1a.appspot.com",
+    messagingSenderId: "682712438829",
+    appId: "1:682712438829:web:86777c4d1ab325c08e40a4"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  ReactDOM.render(<App />, document.getElementById("root"));
+  
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
